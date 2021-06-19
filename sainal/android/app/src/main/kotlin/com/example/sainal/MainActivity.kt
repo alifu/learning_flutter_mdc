e629daef03b5e2c647659d3f44b4baa78d13b1d0
sainalsultan <sainalsultan@gmail.com>
2021-06-19 20:11:04 +0800

    sainal branch

--- sainal/android/app/src/main/kotlin/com/example/sainal/MainActivity.kt
+++ sainal/android/app/src/main/kotlin/com/example/sainal/MainActivity.kt
@@ -3,4 +3,5 @@ package com.example.sainal
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+
 }
--- sainal/lib/main.dart
+++ sainal/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -22,7 +23,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Sainal Home Page'),
     );
   }
 }
@@ -71,6 +72,17 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
+        elevation: 0.0,
+        actions :<Widget> [
+          IconButton(
+          icon: Icon(
+            Icons.settings,
+            color:Colors.white,
+        ),
+        onPressed: (){
+
+        },)
+        ],
         title: Text(widget.title),
       ),
       body: Center(
